709a088a0133f4e462f61a92c0bf02e319885be6
ETKuppens <et.kuppens@gmail.com>
2024-11-26 16:22:00 +0100

    Add initial Stubs

---
+++ src/main/java/LabelledTransitionSystem.java
@@ -0,0 +1,9 @@
+
+/**
+ * TODO: Implement LabelledTransitionSystem!
+ * -> implement the operations on an LTS
+ * -> implement a parser from Aldebran either in a separate class or in the constructor
+ */
+public class LabelledTransitionSystem {
+
+}
---
+++ src/main/java/MuCalculusFormula.java
@@ -0,0 +1,9 @@
+
+/**
+ * TODO: Implement MuCalculusFormula!
+ * -> implement the operations on a mu-calculus formula
+ * -> implement a parser from text either in a separate class or in the constructor
+ */
+public class MuCalculusFormula {
+
+}
