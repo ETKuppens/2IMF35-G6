1a9afb9d874f23f5a0159b6cbf50cf032b7e216a
Sjoerd_Halmans <sjoerd.halmans@gmail.com>
2024-12-01 18:54:58 +0100

    Added vertex class

---
+++ src/main/java/Vertex.java
@@ -0,0 +1,11 @@
+public class Vertex {
+    public String StartState;
+    public String Label;
+    public String EndState;
+
+    public Vertex(String startState, String label, String endState) {
+        StartState = startState;
+        Label = label;
+        EndState = endState;
+    }
+}
